da8d895b64342db55e496d452e55c129526d7a97
amanialosaimi <amanialossaimi@gmail.com>
2023-02-21 19:50:56 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Amani Al Osaimi on 01/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
